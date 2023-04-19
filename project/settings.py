d4ece540264836d34ae56337c3262914548f2d56
Job Karani <jobkarani084j@gmail.com>
2023-04-19 08:56:32 +0300

    feat: stripe

--- project/settings.py
+++ project/settings.py
@@ -118,6 +118,10 @@ UPLOADCARE = {
     'secret': config('secret'),
 }
 
+STRIPE_PUBLIC_KEY = config('STRIPE_PUBLIC_KEY')
+STRIPE_SECRET_KEY = config('STRIPE_SECRET_KEY')
+
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
